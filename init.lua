3d0b1e417e68c1fb8ddd654c270190bdd19c02bc
Ufuk Ustali <ustaliufuk73@gmail.com>
2025-01-07 04:48:47 +0100

    added side scrolloff

--- init.lua
+++ init.lua
@@ -161,6 +161,7 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
