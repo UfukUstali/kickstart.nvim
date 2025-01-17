218532cc696bb8d2441834a663206fd1acd038f4
Ufuk Ustali <ustaliufuk73@gmail.com>
2025-01-17 09:14:39 +0100

    fixed lua lsp and stylua

--- lua/snippets/ts-vue.lua
+++ lua/snippets/ts-vue.lua
@@ -1,18 +1,18 @@
-local ls = require("luasnip")
+local ls = require 'luasnip'
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
 return {
-  s("v:base", {
-    t({ '<script setup lang="ts">', "" }),
-    i(1, "put the cursor here"),
-    t({ "", "</script>", "", "", "<template>", "</template>" }),
+  s('v:base', {
+    t { '<script setup lang="ts">', '' },
+    i(1, ''),
+    t { '', '</script>', '', '', '<template>', '</template>' },
   }),
-  s("nitro:event", {
-    t("export default defineEventHandler(async (event) => {"),
-    t({"", "  "}), -- Indentation
-    i(1, "// cursor goes here"), -- Placeholder for the cursor
-    t({"", "});"}),
+  s('nitro:event', {
+    t 'export default defineEventHandler(async (event) => {',
+    t { '', '  ' },
+    i(1, ''),
+    t { '', '});' },
   }),
 }
