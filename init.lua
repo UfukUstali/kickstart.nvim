cb48bcc2cbba80d9f06b2509a8b2a1f886883cc5
Ufuk Ustali <ustaliufuk73@gmail.com>
2025-04-03 00:38:31 +0200

    refactor deprication

--- init.lua
+++ init.lua
@@ -646,11 +646,22 @@ require('lazy').setup({
 
       -- Change diagnostic symbols in the sign column (gutter)
       if vim.g.have_nerd_font then
-        local signs = { Error = '', Warn = '', Hint = '', Info = '' }
-        for type, icon in pairs(signs) do
-          local hl = 'DiagnosticSign' .. type
-          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
+        vim.diagnostic.config {
+          signs = {
+            text = {
+              [vim.diagnostic.severity.ERROR] = '',
+              [vim.diagnostic.severity.WARN] = '',
+              [vim.diagnostic.severity.HINT] = '',
+              [vim.diagnostic.severity.INFO] = '',
+            },
+            numhl = {
+              [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+              [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+              [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+              [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+            },
+          },
+        }
       end
 
       vim.api.nvim_create_autocmd('FileType', {
