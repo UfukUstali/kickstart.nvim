ca566a3a3c71c253cd6d4f8f7afb35dcd903b045
Ufuk Ustali <ustaliufuk73@gmail.com>
2025-10-25 22:27:53 +0200

    style changes for the blink.cmp windows

--- init.lua
+++ init.lua
@@ -515,6 +515,7 @@ require('lazy').setup({
     version = '1.*',
     dependencies = {
       'folke/lazydev.nvim',
+      'onsails/lspkind-nvim',
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -528,8 +529,60 @@ require('lazy').setup({
       },
 
       completion = {
+        menu = {
+          border = 'rounded',
+          draw = {
+            components = {
+              kind = {
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                    local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
+                    if dev_icon then
+                      hl = dev_hl
+                    end
+                  end
+                  return hl
+                end,
+              },
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+                  if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                    local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
+                    if dev_icon then
+                      icon = dev_icon
+                    end
+                  else
+                    icon = require('lspkind').symbolic(ctx.kind, {
+                      mode = 'symbol',
+                    })
+                  end
+
+                  return icon .. ctx.icon_gap
+                end,
+
+                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- You can also add the same function for `kind.highlight` if you want to
+                -- keep the highlight groups in sync with the icons.
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                    local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
+                    if dev_icon then
+                      hl = dev_hl
+                    end
+                  end
+                  return hl
+                end,
+              },
+            },
+          },
+        },
         list = { selection = { auto_insert = false } },
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 500, window = {
+          border = 'rounded',
+        } },
         ghost_text = { enabled = true },
         accept = {
           auto_brackets = { enabled = false },
@@ -550,6 +603,9 @@ require('lazy').setup({
         trigger = {
           show_on_trigger_character = false,
           show_on_insert_on_trigger_character = false,
+        },
+        window = {
+          border = 'rounded',
         },
       },
     },
